14d670d50b1f9d44ec03406291e8986ad10d5d59
thawsoar@gmail.com <taoxiang>
2021-04-29 17:11:21 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,6 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 安装生产环境依赖包
-yarn install --production
-# 压缩项目
-tar -zcvf ./release.tgz .
-mkdir dist
-mv release.tgz dist/
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 git config --global user.email "taoxiang"
